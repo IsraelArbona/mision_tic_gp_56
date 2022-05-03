644a7217b112742cbd8765b79d121be8883b4e0f
IsraelArbona <i.arbonammisena.edu.co>
2022-05-03 16:01:48 -0500

    Clase 03 de mayo del 2022

--- ciclo_1/semana1/ejercicio3.py
+++ ciclo_1/semana1/ejercicio3.py
@@ -16,7 +16,7 @@ def repetir_funciones():
 
 
 
-
+'''
 # Creamos la función operacionSuma de dos variables
 def operacionSuma():
     a = 45
@@ -44,10 +44,98 @@ def sumarDosnumeros():
     print("la suma de", int(num1),"+", int(num2),"es igual a",int(num1 + num2))
 
 # sumarDosnumeros()
+'''
+
 
 def raizCuadrada():
     valor = float(input("Por favor, introduzca un numero a calcular la raiz cuadrada "))
     raiz = valor ** 0.5
     return print("La raiz cuadrada del valor : ", valor, "es",raiz)
 
-raizCuadrada()
+# raizCuadrada()
+
+
+b = 5
+a = 15
+
+def suma(a,b):
+    return a + b
+
+B = 30
+A = 10
+
+#print(suma(A,B))
+#print(suma(a,b))
+
+#resultado = suma(5,3)
+#print(resultado)
+
+def mi_funcion(nombre,apellido):
+    miNombre = nombre + apellido
+    return miNombre
+
+# print(mi_funcion("Luis ", "Morelo"))
+
+def saludar(nombre,mensaje = "Hola"):
+    print(mensaje,nombre)
+
+# saludar("Pepe Grillo")
+
+
+def mensaje():
+    print("Ingrese por favor un valor")
+
+def sumarDosnumeros():
+    mensaje()
+    num1 = float(input())
+    mensaje()
+    num2 = float(input())
+    return print("La suma de", num1,"+",num2,"es igual a:",num1 + num2)
+
+#sumarDosnumeros()
+
+'''
+#Crear la función
+def primeraFuncion(): # función externa
+    print("\n \"Hola desde la función externa\" \n")
+    def segundaFuncion():
+        print("\n \"Hola desde la función interna\" \n")
+
+    #Llamar la función
+    segundaFuncion()
+
+# Llamar la función
+#primeraFuncion()
+'''
+
+
+
+'''
+def primerNumero(x):
+    def segundoNumero(y):
+        return x * y
+    return segundoNumero
+
+resultado = primerNumero(2)
+
+# print(resultado)
+
+print(type(resultado))
+print(type(resultado(5)))
+print(resultado(5))
+'''
+
+def primeraFuncion():
+    x = 2
+
+    #Creando la función segundoFuncion
+    def segundaFuncion(a):
+        x = 6
+        print(a + x)
+    
+    #llamamos la función
+    segundaFuncion(3)
+    print(x)
+
+#llamamos la función
+primeraFuncion()
---
+++ ciclo_1/semana1/ejercicio4.py
@@ -0,0 +1,45 @@
+# crear función
+def nombre_genero(letraGenero):
+    pass # no se va a realizar ninguna secuencia en el función
+    # return letraGenero
+
+# print(nombre_genero("m"))
+
+# realizar una función que permita obtener el primedio de cuatro notas
+# y que el resultado sea un numero entero
+
+nota1 = 3.7
+nota2 = 2.4
+nota3 = 4.7
+nota4 = 3.2
+
+def calcularPromedio1(a,b,c,d):
+    resultado = (a + b + c + d)/4
+    return resultado
+
+print(int(calcularPromedio1(nota1, nota2, nota3, nota4)))
+
+def calcularPromedio2(nota1,nota2,nota3,nota4):
+    promNotas = (nota1 + nota2 + nota3 + nota4)/4
+    return int(promNotas)
+
+print(calcularPromedio2(nota1, nota2, nota3, nota4))
+
+def calcularPromedio3(nt1,nt2,nt3,nt4):
+    result = (nt1+nt2+nt3+nt4)/4
+    return round(result)
+
+print(calcularPromedio3(nota1, nota2, nota3, nota4))
+
+
+# crear función con sum(), len() para calcular el promedio
+def cal_promedio(lista_notas):
+    result = (sum(lista_notas)/len(lista_notas))
+    return int(result)
+
+notas = (3.7,2.4,4.7,3.2)
+notas2 = (3.7,4.5,2.5,3,4,1.6,1)
+
+print(type(notas))
+print(cal_promedio(notas))
+print(cal_promedio(notas2))
