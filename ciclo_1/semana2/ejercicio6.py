8ec2ec4a334a3f6924d7680caa7ce7bc95b0060e
IsraelArbona <i.arbonammisena.edu.co>
2022-05-17 17:29:00 -0500

    Clase 17 de mayo del 2022

--- ciclo_1/semana2/ejercicio6.py
+++ ciclo_1/semana2/ejercicio6.py
@@ -119,3 +119,142 @@ print('diccionario6: ', diccionario6)
     Actuliza un diccionario agregando nuevos pares (clave-valor).
     se llama el update() sin pasar parametros, el diccionario permanece sin cambio.
 '''
+
+diccionario7 = {
+    'Cedula': 1021100,
+    'Nombre': 'Juan',
+    'Apellido': 'Diaz',
+    'Correo': 'jdiaz@micorreo.com'
+}
+
+print('\nDiccionario original: ', diccionario7)
+
+nuevoDiccionario7 = ({'Telefono' : 6019435459})
+print('nuevoDiccionario7 - complemento', nuevoDiccionario7)
+
+diccionario7.update(nuevoDiccionario7)
+print('\n Diccionario actulizado', diccionario7)
+
+
+# ejercicio 8
+
+usuario_1 = {
+    'Cedula': 1021100,
+    'Nombre': 'Juan',
+    'Apellido': 'Diaz',
+    'Correo': 'jdiaz@micorreo.com'
+}
+
+usuario_2 = {
+    'Cedula': 2301223,
+    'Nombre': 'Carlos',
+    'Apellido': 'Marin',
+    'Correo': 'cmarin@micorreo.com'
+}
+
+print('Cliente 1 \n', usuario_1)
+print('Cliente 2 \n', usuario_2)
+
+usuario_1.update(usuario_2)
+
+print('Usuario 1 actulizado: \n', usuario_1)
+
+
+# ejercicio 9
+
+datos = {
+    'nombre': 'Andres',
+    'apellido': 'Pizarro',
+    'cedula': 19343222
+}
+
+for k in datos.keys():
+    print('{} = {}'.format(k,datos[k]))
+
+datos_add = {
+    'cuenta_ahorro': 203249344,
+    'saldo': 10000
+}
+
+print('\nNumero de items: ', len(datos))
+datos.update(datos_add)
+print('Numero de items actulizados: ', len(datos))
+print('\n', datos)
+
+
+# ejercicio 10
+# Método keys() y sorted()
+# Retorna una lista de todas las claves dentro del diccionario ascendentemente
+
+datos10 = {
+    'nombre': 'Andres',
+    'apellido': 'Pizarro',
+    'cedula': 19343222
+}
+
+print()
+
+for key in sorted(datos10.keys()):
+    # print(key)
+    print(key, " -> ", datos10[key])
+
+
+# ejercicio 11
+# Método items() retorna lista de tuplas
+
+datos11 = {
+    'nombre': 'Andres',
+    'apellido': 'Pizarro',
+    'cedula': 19343222
+}
+
+print()
+for k, valor in datos11.items():
+    print(k, ' = ', valor)
+
+# Ejecicio 12
+# Comparaciones de nombre de los estudiantes.
+
+informacion = { 
+    'alumno1':{
+        'nombre': 'David',
+        'edad': 13,
+        'estatura': 1.60,
+        'grado':'Junior'
+    },
+    'alumno2':{
+        'nombre': 'Juan',
+        'edad': 16,
+        'estatura': 1.70,
+        'grado':'Master'
+    } 
+}
+
+print('\n', informacion, '\n')
+
+# comparación de los Nombres de cada alumno
+
+if informacion['alumno1']['nombre'] == informacion['alumno2']['nombre']:
+    print('\n' + str(informacion['alumno1']['nombre']) +
+    ' es igual a '+ str(informacion['alumno2']['nombre']) + '\n')
+else:
+    print('\n' + str(informacion['alumno1']['nombre']) +
+    ' es diferente a '+ str(informacion['alumno2']['nombre']) + '\n')
+
+
+# ejercicio 13
+
+diccionario13 = {
+    'gato': 'chat',
+    'perro': 'chiel',
+    'caballo': 'cheval'
+}
+
+nuevo = ['gato','leon','caballo']
+
+for n in nuevo:
+    if n in diccionario13:
+        print(n, ' -> ', diccionario13[n])
+    else:
+        print(n, ' no esta en el diccionario13')
+
---
+++ ciclo_1/semana3/ejercicio1.py
@@ -0,0 +1,193 @@
+# Ciclos While
+
+'''
+n = 5
+while n > 0: # -> condición para realizar cada iteración
+    print(n)
+    n = n - 1
+print('Fin')
+'''
+
+# break -> nos permite terminar el ciclo.
+
+'''
+while True:    
+    numero = int(input('Ingrese un número: '))
+    if numero % 2 == 0:
+        print('El número es par')
+        break
+    else: 
+        print('El número es impar')
+        break
+'''
+
+'''
+n = int(input('Por favor, ingresa un número mayor que cero: '))
+while n > 0:
+    print(n)
+    print('Correcto!')
+    break
+'''
+
+'''
+n = int(input('Por favor, ingrese un número positivo'))
+
+while n > 0:
+    print(n)
+    n = n-1
+print('Finalizar') 
+'''
+
+# ejercicio 2
+# Alejarse de terminación
+
+'''
+i = 1
+while i > 0:
+    print(i)
+    i += 1
+print('Terminar')
+'''
+
+# ejercicio 3
+# Brincarse la meta
+
+'''
+i = 1
+while i != 10:
+    print(i)
+    i += 2
+print('Fin')
+'''
+
+# ejercicio 4
+# Problemas de indentación
+
+'''
+n = 1
+while n<10:
+    print(n)
+n = n+1
+print('Finalizar')
+'''
+
+# ejercicio 5
+# olvidar el avance
+
+'''
+i = 1
+while i<10:
+    print(i)
+print('Finalizar')
+'''
+
+# ejercicio 6
+# Mostrar los primero 40 número de 100
+
+'''
+n = 1
+while n<=100:
+    print(n)
+    if n == 40:
+        break
+    n += 1
+print('Finalizar')
+'''
+
+'''
+n = 1
+while n < 100:
+    if n > 0 and n <= 40:
+        print(n)
+    else:
+        break
+    n = n + 1
+'''
+
+# ejercicio 7
+# continue -> omitir las lineas siguientes de bloque del ciclo.
+
+# mostrar los primero 5 número impares saltando el cuarto número.
+
+'''
+    1
+    3
+    5
+
+    9
+    11
+'''
+
+'''
+n = -1
+while n != 11:
+    n = n + 2
+    if n == 7:
+        continue
+    print(n)
+'''
+
+'''
+var = -1
+while var<11:
+    var += 2
+    if var == 7:
+        continue
+    print('Valor variable es: ', var)
+'''
+
+'''
+i = 0 
+impar = -1
+while i <= 5:
+    impar += 2
+    i += 1
+    if i == 4:
+        continue
+    print(impar)
+'''
+
+# ejercicio 8 
+# Bucle while controlado por evento
+
+'''
+acuNotas, promedio, conNotas = 0,0.0,0
+
+print('Introduzca la nota de un estudiante ( -1 para salir): ')
+calif = int(input())
+
+while calif != -1:
+    acuNotas += calif
+    conNotas += 1
+    print('Introduzca la nota de un estudiante ( -1 para salir): ')
+    calif = int(input())
+
+promedio = acuNotas/conNotas
+print('Promedio de notas de los estudiantes es ' + str(promedio))
+'''
+
+'''
+acuNotas, promedio, conNotas = 0,0.0,0
+mensaje = 'Introduzca la nota de un estudiante ( -1 para salir): '
+calif = int(input(mensaje))
+
+while calif != -1:
+    acuNotas += calif
+    conNotas += 1
+    calif = int(input(mensaje))
+else:
+    if acuNotas != 0:
+        promedio = round(acuNotas/conNotas,1)
+        print('Promedio de notas de los estudiantes es ' + str(promedio))
+    else:
+        print('No existen Notas')
+'''
+
+a, b = 0, 1
+while b < 100:
+    print(b)
+    a, b = b , a + b
+
+
+
+
